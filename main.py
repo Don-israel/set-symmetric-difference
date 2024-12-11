10a86b01326d594b233f85c15baa43dd81e0087e
Don_israel <israelojakorotu@gmail.com>
2024-12-11 12:36:07 +0100

    symmetric difference

---
+++ main.py
@@ -0,0 +1,9 @@
+# Define two sets
+set1 = {"blue", "purple"}
+set2 = {"purple", "pink"}
+
+# Find the symmetric difference
+result = set1.symmetric_difference(set2)
+
+# Print the result
+print("The symmetric difference between the two sets is:", result)
